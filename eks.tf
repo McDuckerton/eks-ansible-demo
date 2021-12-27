ddea23c6809be3c17c3726eb4a8ea82d96f238bc
McLeod, Chris <28598851+McDuckerton@users.noreply.github.com>
2021-12-27 14:32:08 -0600

    formatting update

--- eks.tf
+++ eks.tf
@@ -1,24 +1,30 @@
 module "eks" {
-  source = "terraform-aws-modules/eks/aws"
-  cluster_name = local.cluster_name
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = local.cluster_name
   cluster_version = "1.20"
-  subnets = module.vpc.private_subnets
+  subnets         = module.vpc.private_subnets
 
   tags = {
     Name = "${local.cluster_name}"
   }
-  
+
+  vpc_id = module.vpc.vpc_id
+
+  workers_group_defaults = {
+    root_volume_type = "gp2"
+  }
+
   worker_groups = [
     {
-      name = "worker-group-1"
-      instance_type = "t2.micro"
-      asg_desired_capacity = 2
+      name                          = "worker-group-1"
+      instance_type                 = "t2.micro"
+      asg_desired_capacity          = 2
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
     },
     {
-      name = "worker-group-2"
-      instance_type = "t2.micro"
-      asg_desired_capacity = 1
+      name                          = "worker-group-2"
+      instance_type                 = "t2.micro"
+      asg_desired_capacity          = 1
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
     }
   ]
